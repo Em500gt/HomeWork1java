c88c2dc0fe47d56fdf5c049d809e1351a95c6482
Em500gt <47947298+Em500gt@users.noreply.github.com>
2023-01-23 22:24:54 +0300

    Update Task3.java

--- Task3.java
+++ Task3.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Task3 {
 
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в калькулятор.\nВыход -> Stop");
+        System.out.println("Добро пожаловать в калькулятор.");
         Scanner iScanner = new Scanner(System.in);
 
         boolean status = true;
